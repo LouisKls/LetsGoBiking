82621eca1e409ff16fa2a0ef17b8ba196a5ad323
Louis Calas <louiskls.dev@gmail.com>
2022-12-08 23:47:14 +0100

    Add files via upload

---
+++ RoutingServer_Host/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace RoutingServer_Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(RoutingServer.RoutingService));
+            host.Open();
+            Console.WriteLine("Service Hosted Sucessfully");
+            Console.Read();
+
+        }
+    }
+}
